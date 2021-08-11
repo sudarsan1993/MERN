46c43836ac6ff818a8ed897a2103bdd76d57a00e
sudarsan1993 <suadrsanreddy1125@gmail.com>
2021-08-11 18:53:24 +0530

    React-front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homescreen from "./screens/Homescreen";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to EasyShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Homescreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
