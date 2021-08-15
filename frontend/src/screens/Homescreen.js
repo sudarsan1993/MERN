ec43154f10eca431ba5ca3e163b1c88f51e8cd84
sudarsan1993 <suadrsanreddy1125@gmail.com>
2021-08-15 15:29:38 +0530

    third commit

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import products from "../products";
+import axios from "axios";
 
 const Homescreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchproducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchproducts();
+  }, []);
+
   return (
     <>
       <h1>Latest products</h1>
